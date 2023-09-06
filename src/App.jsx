18f572edde6788192f36c870987a0c895f4149f6
bonbon <nepunepubon@gmail.com>
2023-09-06 22:22:51 +0800

    Changed button behaviour

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>pressing the button now increments number by 2</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 2)}>
           count is {count}
         </button>
         <p>
